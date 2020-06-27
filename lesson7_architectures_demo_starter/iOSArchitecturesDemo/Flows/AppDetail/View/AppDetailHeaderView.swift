b81b6da0b0175fabf8c86df4d3b72194fa290689
Jasik <rogozhkin87@gmail.com>
2020-06-27 22:24:50 +0900

    added Proj

---
+++ lesson7_architectures_demo_starter/iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailHeaderView.swift
@@ -0,0 +1,11 @@
+//
+//  AppDetailHeaderView.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Vladimir Rogozhkin on 2020/06/27.
+//  Copyright © 2020 ekireev. All rights reserved.
+//
+
+import UIKit
+
+
